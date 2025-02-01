0ef56569cc7d3093a5287d6b50093f605a471f5b
PabloFC <pablofc_68@hotmail.com>
2025-02-01 21:03:30 +0100

    rutas relacionadas a usuarios

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ const app = express();
 app.use(express.static("public"));
 
 // Rutas
-// app.get("/", (req, res) => {
-//   res.json({
-//     ok: true,
-//   });
-// });
-
+app.use("/api/auth", require("./routes/auth"));
 // escuchar peticiones
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);

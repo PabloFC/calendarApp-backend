9d9418cf4d0354066fc1121aab5d9cf7e672d0f6
PabloFC <pablofc_68@hotmail.com>
2025-02-02 20:30:30 +0100

    recuperar info de un posteo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,34 @@
 const { response } = require("express");
 
 const crearUsuario = (req, res = response) => {
+  const { name, email, password } = req.body;
+
+  if (name.length < 5) {
+    return res.status(400).json({
+      ok: false,
+      msg: "El nombre debe de ser de 5 letras",
+    });
+  }
+
   res.json({
     ok: true,
     msg: "crearUsuario",
+    name,
+    email,
+    password,
   });
 };
 
-const loginUsuario = (req, res) => {
+const loginUsuario = (req, res = response) => {
   res.json({
     ok: true,
     msg: "login",
+    email,
+    password,
   });
 };
 
-const revalidarToken = (req, res) => {
+const revalidarToken = (req, res = response) => {
   res.json({
     ok: true,
     msg: "renew",
